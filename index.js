f2c42b6a2e215bd45d541a9ae110010afbd20a0e
MOGILEESWAR <47683272+mogili1402@users.noreply.github.com>
2023-04-15 22:25:38 +0530

    Update index.js

--- index.js
+++ index.js
@@ -4,4 +4,31 @@ app.all('/', (req, res) => {
     console.log("Just got a request!")
     res.send('Yo!')
 })
+const { MongoClient } = require('mongodb');
+// or as an es module:
+// import { MongoClient } from 'mongodb'
+
+// Connection URL
+const url = 'mongodb://u9h10biymcwx8lbnlrq1:8GxqndT5sJdLKZTKxGB0@n1-c2-mongodb-clevercloud-customers.services.clever-cloud.com:27017,n2-c2-mongodb-clevercloud-customers.services.clever-cloud.com:27017/b06og4ims3wfkpa?replicaSet=rs0';
+const client = new MongoClient(url);
+
+// Database Name
+const dbName = 'b06og4ims3wfkpa';
+
+async function main() {
+  // Use connect method to connect to the server
+  await client.connect();
+  console.log('Connected successfully to server');
+  const db = client.db(dbName);
+  const collection = db.collection('documents');
+
+  // the following code examples can be pasted here...
+
+  return 'done.';
+}
+
+main()
+  .then(console.log)
+  .catch(console.error)
+  .finally(() => client.close());
 app.listen(process.env.PORT || 3000)
